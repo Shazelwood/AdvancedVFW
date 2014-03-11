8f8eca79a04eaee963f9769e494211d8c75f1ca4
Shae Hazelwood <Hazelwood@Shaes-MacBook-Pro-2.local>
2014-03-10 22:44:54 -0400

    Merge branch 'master' of https://github.com/Shazelwood/AdvancedVFW
    Conflicts:
    NYTimesGEO/.settings/com.appcelerator.titanium.ios.core.prefs
    NYTimesGEO/Resources/RemoteData.js
    NYTimesGEO/Resources/app.js
    NYTimesGEO/Resources/map.png
    NYTimesGEO/Resources/search.js

--- NYTimesGEO/Resources/RemoteData.js
+++ NYTimesGEO/Resources/RemoteData.js
@@ -1,14 +1,27 @@
+<<<<<<< HEAD
 var search = require("search");
 var mapWin = Ti.UI.createWindow({
 	title : "Map Window",
 	backgroundImage: "map.png"
 });
 exports.mapWin = mapWin;
+=======
+<<<<<<< HEAD
+var mapWin = Ti.UI.createWindow({
+	title : "Map Window"
+});
+exports.mapWin = mapWin;
+=======
+var currentWin = Ti.UI.currentWindow;
+>>>>>>> FETCH_HEAD
+
+>>>>>>> FETCH_HEAD
 var holder = Ti.UI.createView({
 	height: 800,
 	bottom: 0,
 	width: Ti.UI.FILL,
 });
+<<<<<<< HEAD
 var refreshHolder = Ti.UI.createView({
 	borderRadius : '5%',
 	center : '0%',
@@ -22,6 +35,64 @@ var refresh = Ti.UI.createButton({
 refresh.addEventListener("click", function(){
 	xhr.open("GET", url);
 	xhr.send();
+=======
+var info = [];
+var response, name, population, lat, lng, dist, county;
+var remoteResponse = function() {
+    // Response function code
+	response = JSON.parse(this.responseText);
+	for (var i = 0; i < response.results.length; i++){
+		name = response.results[i].geocode.name;
+		population = response.results[i].geocode.population;
+		lat = response.results[i].geocode.latitude;
+		lng = response.results[i].geocode.longitude;
+<<<<<<< HEAD
+		dist = Math.round(response.results[i].geocode.distance/100)*100;
+=======
+		dist = response.results[i].geocode.distance;
+>>>>>>> FETCH_HEAD
+		county = response.results[i].geocode.admin_name2;
+		info.push({
+			id: i + 1,
+			name: name,
+			county: county,
+			population: population,
+			distance: dist,
+			longitude: lng,
+			latitude: lat,
+		});
+	};
+	var Map = require('ti.map');
+	var mapview = Map.createView({mapType:Map.NORMAL_TYPE});
+<<<<<<< HEAD
+	mapWin.add(mapview);
+	// console.log(info);
+};
+exports.info = info;
+=======
+	currentWin.add(mapview);
+	console.log(info);
+	
+};
+
+>>>>>>> FETCH_HEAD
+
+var remoteError = function(e) {
+    Ti.API.debug("Status: " + this.status);
+    Ti.API.debug("Text: " + this.responseText);
+    Ti.API.debug("Error: " + e.error);
+<<<<<<< HEAD
+    alert("There is a problem pulling the data");
+=======
+    alert("There's a problem pulling remote data");
+>>>>>>> FETCH_HEAD
+};
+
+var xhr = Ti.Network.createHTTPClient({
+    onload: remoteResponse,
+    onerror: remoteError,
+    timeout:5000
+>>>>>>> FETCH_HEAD
 });
 
 var runGeo = function(){
@@ -107,6 +178,34 @@ var runGeo = function(){
 	});
 	
 };
+<<<<<<< HEAD
 
 runGeo();
 mapWin.add(holder);
+=======
+<<<<<<< HEAD
+=======
+var mapview = Titanium.Map.createView({
+    mapType: Titanium.Map.STANDARD_TYPE,
+    region:{latitude:33.74511, longitude:-84.38993, latitudeDelta:0.5, longitudeDelta:0.5},
+    animate:true,
+    regionFit:true,
+    userLocation:true
+});
+currentWin.add(mapview);
+>>>>>>> FETCH_HEAD
+runGeo();
+
+
+
+xhr.open("GET", "http://api.nytimes.com/svc/semantic/v2/geocodes/query.json?nearby=37.78583526611328,-122.40641784667969&api-key=ec2c67ba0d91240ac18bbf24043c8cb5:1:68792990");
+xhr.send();
+
+
+
+<<<<<<< HEAD
+mapWin.add(holder);
+=======
+currentWin.add(holder);
+>>>>>>> FETCH_HEAD
+>>>>>>> FETCH_HEAD
--- NYTimesGEO/Resources/search.js
+++ NYTimesGEO/Resources/search.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 var dataB = Ti.Database.open('GeoDB');
 dataB.execute('CREATE TABLE IF NOT EXISTS location(id INTEGER PRIMARY KEY, name TEXT, population TEXT, longitude INTEGER, latitude INTEGER, distance TEXT, county TEXT, state TEXT)');
+=======
+//requiring JS files
+var box = require('storage');
+var map = require('RemoteData');
+>>>>>>> FETCH_HEAD
 
 //Window Start
 var sWin = Ti.UI.createWindow({
@@ -9,6 +15,7 @@ var sWin = Ti.UI.createWindow({
 exports.sWin = sWin;
 //Window End
 
+<<<<<<< HEAD
 //Search Bar Start
 var searchbar = Ti.UI.createSearchBar({
 	top : 0,
@@ -162,4 +169,101 @@ timesTable.addEventListener('click', function(e) {
 });
 
 sWin.add(timesTable);
+=======
+//save button start
+var saveBTN = Ti.UI.createButton({
+	title : 'SAVE',
+	bottom : '5%',
+	center : '0%'
+});
+
+// saveBTN.addEventListener('click', box.save);
+//save button end
+
+var cancelBTN = Ti.UI.createButton({
+	systemButton : Ti.UI.iPhone.SystemButton.CANCEL,
+	title : 'CANCEL',
+	top : '5%',
+	left : '5%'
+});
+
+cancelBTN.addEventListener('click', function() {
+	sWin.close();
+});
+
+var name, population, lat, lng, dist, county; 
+
+name = map.info.name;
+console.log(name);
+
+//Labels Begin
+
+var titleView = Ti.UI.createLabel({
+	top : '50%',
+	left : '10%',
+	text : name,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 20,
+		fontWeight : 'bold'
+	}
+});
+
+var countyLabel = Ti.UI.createLabel({
+	top : '55%',
+	left : '10%',
+	text : county,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 16
+	}
+});
+
+var latLabel = Ti.UI.createLabel({
+	top : '60%',
+	left : '10%',
+	text : 'lat: ' + latitude,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 16
+	}
+});
+
+var lngLabel = Ti.UI.createLabel({
+	top : '65%',
+	left : '10%',
+	text : 'lng: ' + longitude,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 16
+	}
+});
+
+var countryLabel = Ti.UI.createLabel({
+	top : '70%',
+	left : '10%',
+	right : '15%',
+	text : country,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 16
+	}
+});
+
+var distLabel = Ti.UI.createLabel({
+	top : '75%',
+	left : '10%',
+	right : '15%',
+	text : 'Dist: ' + dist,
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : 16
+	}
+});
+//Labels End
+
+//EventListener Main Code
+sWin.add(cancelBTN, saveBTN);
+// evtWin.add(mapView, titleView, countyLabel, latLabel, lngLabel, countryLabel, distLabel);
+>>>>>>> FETCH_HEAD
 sWin.open();
